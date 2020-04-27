72142558b56cd2787213da8f27041f4e8dcf6392
martincazares <jm.cazaresg@gmail.com>
2020-04-27 09:55:36 -0500

    add random id for non action notifications...

--- app/src/main/java/mx/cubiccoding/front/notifications/NotificationPayloadAction.kt
+++ app/src/main/java/mx/cubiccoding/front/notifications/NotificationPayloadAction.kt
@@ -18,6 +18,7 @@ import mx.cubiccoding.front.home.scoreboard.questions.TestActivity.Companion.TES
 import mx.cubiccoding.front.splash.Splash
 import mx.cubiccoding.model.utils.Constants
 import mx.cubiccoding.persistence.preferences.UserPersistedData
+import java.util.concurrent.ThreadLocalRandom
 
 class NotificationPayloadAction(
     private val title: String,
@@ -36,12 +37,17 @@ class NotificationPayloadAction(
         if (!UserPersistedData.isLogged) return//Nothing to do if the user is not logged in...
 
         val context = CubicCodingApplication.instance
+        var notificationIcon = R.drawable.ic_cc_no_bg
+
+        //Setup the notification action, icon, and other properties...
         val intent = when(action) {
                 Constants.NEW_SCORE_TEST_ACTION_VALUE -> {
                     Intent(context, Home::class.java).apply {
                         action = OPEN_BOTTOM_QUESTION_FRAGMENT_ACTION
                         putExtra(TEST_UUID_PRE_POPULATED_KEY, pushData)
                         flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        //A new specific icon has to be specified for new test score...
+                        notificationIcon = R.drawable.ic_question_answer
                     }
                 }
                 else -> {
@@ -51,7 +57,7 @@ class NotificationPayloadAction(
         val snoozePendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
         val builder = NotificationCompat.Builder(context, BASIC_NOTIFICATION_CHANNEL_ALL)
-            .setSmallIcon(R.drawable.ic_question_answer)
+            .setSmallIcon(notificationIcon)
             .setContentTitle(title)
             .setContentText(message)
             .setAutoCancel(true)
@@ -59,8 +65,16 @@ class NotificationPayloadAction(
                 .bigText(message))
             .setContentIntent(snoozePendingIntent)
 
-        fireNotification(context, pushData.hashCode(), builder)
+        fireNotification(context, getNotificationId(), builder)
     }
+
+    private fun getNotificationId(): Int =
+        // This logic could be updated based on our needs, for now the default is the hash of the push data or random if no data is provided
+        if (pushData.isNotEmpty()) {
+            pushData.hashCode()
+        } else {
+            ThreadLocalRandom.current().nextInt(1, 1000)
+        }
 
     private fun fireNotification(context: Context, notificationId: Int, builder: NotificationCompat.Builder) {
         with(NotificationManagerCompat.from(context)) {
